f79245f189c9048518d8f992dcde411f41e3df0d
1000304810 <kevinorrego0617gmail.com>
2020-09-30 11:34:13 -0500

    Complete DB

---
+++ MyLeasing.Web/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyLeasing.Web.Data.Entities;
+
+namespace MyLeasing.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        //conexion a la BD
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Contract> contracts { get; set; }
+        public DbSet<Lessee> Lessees { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Property> Properties { get; set; }
+        public DbSet<PropertyImage> propertyImages { get; set; }
+        public DbSet<PropertyType> propertyTypes { get; set; }
+
+    }
+}
---
+++ MyLeasing.Web/Migrations/20200930162136_CompleteDB.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyLeasing.Web.Migrations
+{
+    public partial class CompleteDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FixedPhone",
+                table: "Owners",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Document",
+                table: "Owners",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CellPhone",
+                table: "Owners",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.CreateTable(
+                name: "Lessees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Document = table.Column<string>(maxLength: 20, nullable: false),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    FixedPhone = table.Column<string>(maxLength: 20, nullable: true),
+                    CellPhone = table.Column<string>(maxLength: 20, nullable: true),
+                    Address = table.Column<string>(maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lessees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "propertyTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_propertyTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Properties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Neighborhood = table.Column<string>(maxLength: 50, nullable: false),
+                    Address = table.Column<string>(maxLength: 50, nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    SquareMeters = table.Column<int>(nullable: false),
+                    Rooms = table.Column<int>(nullable: false),
+                    Stratum = table.Column<int>(nullable: false),
+                    HasParkingLot = table.Column<bool>(nullable: false),
+                    IsAvailable = table.Column<bool>(nullable: false),
+                    Remarks = table.Column<string>(nullable: true),
+                    PropertyTypeId = table.Column<int>(nullable: true),
+                    OwnerId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Properties", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Properties_Owners_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Owners",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Properties_propertyTypes_PropertyTypeId",
+                        column: x => x.PropertyTypeId,
+                        principalTable: "propertyTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "contracts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Remarks = table.Column<string>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: false),
+                    OwnerId = table.Column<int>(nullable: true),
+                    LesseeId = table.Column<int>(nullable: true),
+                    PropertyId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contracts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_contracts_Lessees_LesseeId",
+                        column: x => x.LesseeId,
+                        principalTable: "Lessees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_contracts_Owners_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Owners",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_contracts_Properties_PropertyId",
+                        column: x => x.PropertyId,
+                        principalTable: "Properties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "propertyImages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ImageUrl = table.Column<string>(nullable: false),
+                    PropertyId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_propertyImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_propertyImages_Properties_PropertyId",
+                        column: x => x.PropertyId,
+                        principalTable: "Properties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_contracts_LesseeId",
+                table: "contracts",
+                column: "LesseeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_contracts_OwnerId",
+                table: "contracts",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_contracts_PropertyId",
+                table: "contracts",
+                column: "PropertyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Properties_OwnerId",
+                table: "Properties",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Properties_PropertyTypeId",
+                table: "Properties",
+                column: "PropertyTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_propertyImages_PropertyId",
+                table: "propertyImages",
+                column: "PropertyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contracts");
+
+            migrationBuilder.DropTable(
+                name: "propertyImages");
+
+            migrationBuilder.DropTable(
+                name: "Lessees");
+
+            migrationBuilder.DropTable(
+                name: "Properties");
+
+            migrationBuilder.DropTable(
+                name: "propertyTypes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FixedPhone",
+                table: "Owners",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Document",
+                table: "Owners",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CellPhone",
+                table: "Owners",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+    }
+}
